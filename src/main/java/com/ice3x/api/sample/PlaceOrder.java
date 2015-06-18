44a263c5c4d795ef2706f827fdf9144df1f17d71
shardayyy <shardayyy@yahoo.com>
2015-06-18 14:27:46 -0400

    added note

--- src/main/java/com/ice3x/api/sample/PlaceOrder.java
+++ src/main/java/com/ice3x/api/sample/PlaceOrder.java
@@ -28,6 +28,24 @@ public class PlaceOrder {
 	private static final String ENCODING = "UTF-8";
 	private static final String ALGORITHM = "HmacSHA512";
 
+
+
+	// Write sample code for all api calls
+	//ice3x.create_order('ZAR', 'BTC', 13000000000, 1000000, 'Bid', 'Limit', '1', handleResponse);
+	//var orderIds=[243463];
+	//ice3x.cancel_order(orderIds, handleResponse);
+
+	//var orderIds=[17463];
+	//ice3x.order_detail(orderIds, handleResponse);
+
+	//ice3x.open_orders('ZAR', 'BTC', 10, 78343, handleResponse);
+
+	//ice3x.order_history('ZAR', 'BTC', 10, 7834, handleResponse);
+
+	//ice3x.trade_history('ZAR', 'BTC', 10, 119834, handleResponse);
+
+	//ice3x.account_balance(handleResponse);
+
 	public static void main(String[] args) throws Exception {
 		String response = "";
 		try {
